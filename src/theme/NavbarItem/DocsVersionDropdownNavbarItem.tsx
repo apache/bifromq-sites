b060f78ce4f43dca3013bc3206f0217aa186a1c4
Yonny Hao <popduke@gmail.com>
2026-09-23 17:41:05 +0800

    fix: remove Tenon development label from navbar

--- src/theme/NavbarItem/DocsVersionDropdownNavbarItem.tsx
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.tsx
@@ -4,9 +4,6 @@ import Original from '@theme-original/NavbarItem/DocsVersionDropdownNavbarItem';
 
 export default function DocsVersionDropdownNavbarItem(props: ComponentProps<typeof Original>): ReactNode {
   const { pathname } = useLocation();
-  if (pathname.startsWith('/tenon/docs/')) {
-    return <span className={props.mobile ? 'menu__link' : 'navbar__item'}>Tenon · Development</span>;
-  }
   if (pathname === '/tenon' || pathname.startsWith('/tenon/')) return null;
   return <Original {...props} />;
 }
